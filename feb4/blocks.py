a8d27859c29a95133b83fba3c0f8d536c69c1ccd
rhabur25 <rhabur25@lakers.mercyhurst.edu>
2026-02-04 23:21:46 +0000

    commit3

--- feb4/blocks.py
+++ feb4/blocks.py
@@ -1,12 +1,16 @@
 import gradio as gr
 
-def f(x,y):
+def f(x, y):
     return x + y
 
 with gr.Blocks() as iface:
-    app_input = gr.Number(label = "Type a number")
-    app_input2 = gr.Number(label = "Type another number")
-    app_output = gr.Number(label = "This is the sum of the two numbers")
+    with gr.Row():
+        with gr.Column():
+            app_input = gr.Number(label = "Type a number")
+            app_input2 = gr.Number(label = "Type another number")
+        with gr.Column():
+            app_output = gr.Number(label = "This is the sum of the two numbers")
+
     app_input.change(fn=f, inputs=[app_input, app_input2], outputs=[app_output])
 
 iface.launch()
